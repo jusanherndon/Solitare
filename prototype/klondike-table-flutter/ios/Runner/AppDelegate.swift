c06c68e89b4d13feaf8d282a0d9d2251fb1b4d8b
Justin Herndon <jherndon111@gmail.com>
2026-09-03 09:38:16 -0400

    adding a phone for manual testing

--- prototype/klondike-table-flutter/ios/Runner/AppDelegate.swift
+++ prototype/klondike-table-flutter/ios/Runner/AppDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private var hostChannel: FlutterMethodChannel?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,5 +14,38 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    let channel = FlutterMethodChannel(
+      name: "klondike/host",
+      binaryMessenger: engineBridge.applicationRegistrar.messenger()
+    )
+    channel.setMethodCallHandler { call, result in
+      switch call.method {
+      case "filesDir":
+        let urls = FileManager.default.urls(
+          for: .applicationSupportDirectory,
+          in: .userDomainMask
+        )
+        let dir = urls[0]
+        try? FileManager.default.createDirectory(
+          at: dir,
+          withIntermediateDirectories: true
+        )
+        result(dir.path)
+      case "open":
+        guard let urlString = call.arguments as? String,
+              let url = URL(string: urlString)
+        else {
+          result(
+            FlutterError(code: "open", message: "bad url", details: nil)
+          )
+          return
+        }
+        UIApplication.shared.open(url)
+        result(nil)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
+    hostChannel = channel
   }
 }
